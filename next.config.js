820e3c2ff8fce400a449eaab22ec794b0e9e63b9
Marcos Rosti <rosti_marcos@hotmail.com>
2023-03-21 13:39:17 -0300

    ultima

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
       backendURL: 'nodesendservidor-production.up.railway.app/',
-      frontendURL: 'https://node-send-cliente-beige.vercel.app'
+      frontendURL: 'https://node-send-cliente-beige.vercel.app/'
   }
 }
