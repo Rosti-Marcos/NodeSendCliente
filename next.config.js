db0977baca55b14f42d68dbf72dab02b9324a737
Marcos Rosti <rosti_marcos@hotmail.com>
2023-03-21 11:50:57 -0300

    Primer Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  env: {
+      backendURL: 'https://node-send-servidor.vercel.app',
+      frontendURL: 'https://nodesend-cliente-wine-six.vercel.app'
+  }
 }
-
-module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import React from 'react';
+import AuthState from '../context/auth/authState';
+import AppState from '../context/app/appState';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <AuthState>
+            <AppState>
+                <Component {...pageProps} />
+            </AppState>
+        </AuthState>
+    )
 }
+export default MyApp;
