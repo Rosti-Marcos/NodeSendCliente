810d0939e92c1e18b4f79949dbc4c45bb5aa10d6
Marcos Rosti <rosti_marcos@hotmail.com>
2023-03-21 13:23:41 -0300

    cambiar variables 9

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-      backendURL: 'nodesendservidor-production.up.railway.app',
+      backendURL: 'nodesendservidor-production.up.railway.app/',
       frontendURL: 'https://node-send-cliente-anbe8byr7-rosti-marcos.vercel.app'
   }
 }
